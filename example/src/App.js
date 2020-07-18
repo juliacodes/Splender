516bd413afa3d36c4c44d7b5b3227eb8a1a100e8
juliajcodes <john2122@purdue.edu>
2020-07-18 13:07:28 -0400

    initializing create-react-library

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'splender'
+import 'splender/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text='Create React Example 😄' />
+}
+
+export default App
